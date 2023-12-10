d8eb3c97844944213653ddb25ee0a8fb22331aa2
Rodrigo <rodrigo.matias.1000@gmail.com>
2023-12-10 15:40:42 +0000

    updates on main page style

--- src/components/LocationCard.jsx
+++ src/components/LocationCard.jsx
@@ -4,37 +4,48 @@ function LocationCard() {
             <div className="card-container">
                 
                 <div className="card">
-                    <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
-                    <h3>New York City</h3>
-                    <p>United States Of America </p>
-                    <p> 20/05 2019 - 05/06/2019</p>
+                    <div className="card-img-box">
+                        <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
+                    </div>
+                    <div className="card-content">  
+                        <h3>New York City</h3>
+                        <p>United States Of America </p>
+                        <p> 20/05 2019 - 05/06/2019</p>
+                    </div>
                 </div>
 
                 <div className="card">
-                    <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
-                    <h3>New York City</h3>
-                    <p>United States Of America </p>
-                    <p> 20/05 2019 - 05/06/2019</p>
+                    <div className="card-img-box">
+                        <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
+                    </div>
+                    <div className="card-content">  
+                        <h3>New York City</h3>
+                        <p>United States Of America </p>
+                        <p> 20/05 2019 - 05/06/2019</p>
+                    </div>
                 </div>
 
                 <div className="card">
-                    <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
-                    <h3>New York City</h3>
-                    <p>United States Of America </p>
-                    <p> 20/05 2019 - 05/06/2019</p>
-                </div>
-                <div className="card">
-                    <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
-                    <h3>New York City</h3>
-                    <p>United States Of America </p>
-                    <p> 20/05 2019 - 05/06/2019</p>
-                </div>
+                    <div className="card-img-box">
+                        <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
+                    </div>
 
+                    <div className="card-content">  
+                        <h3>New York City</h3>
+                        <p>United States Of America </p>
+                        <p> 20/05 2019 - 05/06/2019</p>
+                    </div>
+                </div>
+                
                 <div className="card">
-                    <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
-                    <h3>New York City</h3>
-                    <p>United States Of America </p>
-                    <p> 20/05 2019 - 05/06/2019</p>
+                    <div className="card-img-box">
+                        <img src= "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/aa/b1/79/caption.jpg?w=1200&h=-1&s=1" alt="image of NYC"></img>
+                    </div>
+                    <div className="card-content">  
+                        <h3>New York City</h3>
+                        <p>United States Of America </p>
+                        <p> 20/05 2019 - 05/06/2019</p>
+                    </div>
                 </div>
 
 
